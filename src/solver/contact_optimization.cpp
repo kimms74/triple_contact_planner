ba4c73c869bc0caf5e93b0232a7efcdeb1e47d52
ms <audtn7498@naver.com>
2020-09-14 21:02:24 +0900

    devided top bottom

--- src/solver/contact_optimization.cpp
+++ src/solver/contact_optimization.cpp
@@ -122,15 +122,16 @@ namespace suhan_contact_planner
     //               Eigen::Vector3d(0, 0, 9.8) * model_->getMass(); // TODO: Check this
     b.head<3>() =  Eigen::Vector3d(0, 0, 9.8) * model_->getMass(); // TODO: Check this
     // TODO: Momentum + b(3~5)
-    for (size_t i = contact_bottom_number; i < contact_number; i++)
+
+    for (int i = 0; i < contact_number -contact_bottom_number; i++)
     {
       A.block<3, 3>(0, i * 6).setIdentity();
       A.block<3, 3>(3, i * 6) = cross_skew(contacts[i]->getContactTransform().translation());
       A.block<3, 3>(3, i * 6 + 3).setIdentity();
     }
 
-    A.block<3, 3>(0,(contact_number +1)*6).setIdentity();
-    A.block<3, 3>(3, (contact_number +1)*6 + 3).setIdentity();
+    A.block<3, 3>(0,(contact_number -contact_bottom_number)*6).setIdentity();
+    A.block<3, 3>(3, (contact_number -contact_bottom_number)*6 + 3).setIdentity();
 
     eq_constraint->setA(A);
     eq_constraint->setEqualityCondition(b);
@@ -183,12 +184,13 @@ namespace suhan_contact_planner
 
     }
 
-    Eigen::VectorXd d_max_n;
-    d_max_n << normal_force, -normal_force;
+    Eigen::VectorXd d_max_n(2);
+    d_max_n(0)=normal_force;
+    d_max_n(1)=-normal_force;
     Eigen::MatrixXd d_n;
     d_n.setZero(12,1);
     d_n.block<2, 1>(4,0)= d_max_n;
-
+    
     C_all.block<12,6>((contact_number -contact_bottom_number)*12,(contact_number -contact_bottom_number)*6) = C_i;
     d_all.segment<12>((contact_number -contact_bottom_number)*12) = d_n;
     
@@ -198,7 +200,7 @@ namespace suhan_contact_planner
     // std::cout << std::endl;
     solver_top.addConstraint(ineq_constraint);
     // solver.setContactNumber(model_->getContactNumber() -1);
-    solver_top.setContactNumber(contact_number -contact_bottom_number);
+    solver_top.setContactNumber(contact_number -contact_bottom_number+1);
   }
 
   bool ContactOptimization::solve()
@@ -207,92 +209,14 @@ namespace suhan_contact_planner
     ContactOptimizationSolver solver_top;
 
     const int contact_number = model_->getContactNumber();
-    const int contact_bottom_number = 3;
+    const int contact_bottom_number = 2;
     std::vector<ContactPtr> contacts;
     contacts = model_->getContactRobot();
 
     if (contact_number == 0)
       return false;
 
-    // setBottom(contact_number, contact_bottom_number, contacts, solver_bottom);
-    
-    auto eq_constraint = std::make_shared<ConstraintEquality>();
-    // eq_constraint.
-    Eigen::MatrixXd A;
-    Eigen::VectorXd b;
-
-    // TODO: gravity
-
-    A.setZero(6, contact_bottom_number * 6);
-    b.setZero(6);
-    // b.head<3>() = model_->getTransform().linear() *
-    //               Eigen::Vector3d(0, 0, 9.8) * model_->getMass(); // TODO: Check this
-    b.head<3>() =  Eigen::Vector3d(0, 0, 9.8) * model_->getMass(); // TODO: Check this
-    // TODO: Momentum + b(3~5)
-    for (size_t i = 0; i < contact_bottom_number; i++)
-    {
-      A.block<3, 3>(0, i * 6).setIdentity();
-      A.block<3, 3>(3, i * 6) = cross_skew(contacts[i]->getContactTransform().translation());
-      A.block<3, 3>(3, i * 6 + 3).setIdentity();
-    }
-    eq_constraint->setA(A);
-    eq_constraint->setEqualityCondition(b);
-
-    solver_bottom.addConstraint(eq_constraint);
-
-    // every contact
-    auto ineq_constraint = std::make_shared<ConstraintInequality>();
-
-    Eigen::MatrixXd C_all;
-    Eigen::VectorXd d_all;
-    d_all.resize(12*contact_bottom_number);
-
-    C_all.setZero(12 * contact_bottom_number, 6 * contact_bottom_number);
-    // d_all.setZero(contact_number * 12);
-
-    Eigen::MatrixXd C_max[6];
-    Eigen::VectorXd d_max[6];
-    const auto &f_limit = robot_->getForceLimit();
-
-    for (int i = 0; i < 6; i++)
-    {
-      C_max[i].setZero(2, 6);
-      C_max[i](0, i) = 1;
-      C_max[i](1, i) = -1;
-
-      d_max[i].resize(2);
-      d_max[i] = f_limit.col(i);
-    }
-
-    Eigen::MatrixXd C_i(12, 6), d_i(12, 1);
-
-    for (int i = 0; i < 6; i++)
-    {
-      C_i.block<2, 6>(i * 2, 0) = C_max[i];
-      d_i.block<2, 1>(i * 2, 0) = d_max[i];
-    }
-
-    for (int i =0; i < contact_bottom_number; i++)
-    {
-      Eigen::Matrix<double, 6, 6> R_hat;
-      Eigen::Matrix<double, 3, 3> R;
-      R = contacts[i]->getContactTransform().linear().transpose();
-      R_hat.setZero();
-      R_hat.block<3, 3>(0, 0) = R;
-      R_hat.block<3, 3>(3, 3) = R;
-
-      C_all.block<12, 6>(i * 12, i * 6) = C_i * R_hat;
-      d_all.segment<12>(i * 12) = d_i;
-
-    }
-    
-    ineq_constraint->setA(C_all);
-    ineq_constraint->setOnlyLowerBound(d_all);
-    // ineq_constraint->printCondition();
-    // std::cout << std::endl;
-    solver_bottom.addConstraint(ineq_constraint);
-    // solver.setContactNumber(model_->getContactNumber() -1);
-    solver_bottom.setContactNumber(contact_bottom_number);
+    setBottom(contact_number, contact_bottom_number, contacts, solver_bottom);
 
     Eigen::VectorXd result_bottom;
     Eigen::VectorXd result_top;
@@ -319,22 +243,24 @@ namespace suhan_contact_planner
       // d_test.setZero(6,1);
       // contacts[contact_bottom_number]->setContactForceTorque(d_test);
 
-      // double normal_force = 0;
-      // for(int i=0; i < contact_bottom_number; i++)
-      // {
-      //   normal_force += result_bottom(i*6+2);
-      // }
+      double normal_force = 0;
+      for(int i=0; i < contact_bottom_number; i++)
+      {
+        normal_force += result_bottom(i*6+2);
+      }
       
-      // setTop(contact_number, contact_bottom_number, contacts, normal_force, solver_top);
+      setTop(contact_number, contact_bottom_number, contacts, normal_force, solver_top);
 
-      // if(solver_top.solve(result_top))
-      // {
-      //   for (int i= contact_bottom_number; i < contact_number; i++ )
-      //   {
-      //     contacts[i]->setContactForceTorque(result_top.segment<6>(i * 6));
-      //   }
-      // }
-
+      if(solver_top.solve(result_top))
+      {
+        
+        for (int i= contact_bottom_number; i < contact_number-contact_bottom_number; i++ )
+        {
+          contacts[i]->setContactForceTorque(result_top.segment<6>(i * 6));
+        }
+        
+      }
+      
       return true;
     }
     return false;
