0d1248079d0c054b0d707d3deb317592f484434e
jiyeongbaek <jy951111@gmail.com>
2021-01-12 15:18:35 +0900

    edit python file

--- qp_one_body.py
+++ qp_one_body.py
@@ -1,11 +1,36 @@
 from triple_contact_planner import python_wrapper_cpp
 import numpy as np
+import yaml
+
+class ContinuousGraspCandid():
+    def __init__(self):
+        self.file_path = 'ikea_stefan_without_bottom_cont_grasp.yaml'
+        with open(self.file_path, 'r') as stream:
+            self.yaml = yaml.safe_load(stream)
+
+    def get_grasp(self, index, ratio):
+        lb = np.array(self.yaml["grasp_points"][index]['lower_bound'])
+        ub = np.array(self.yaml["grasp_points"][index]['upper_bound'])
+        ori = np.array(self.yaml["grasp_points"][index]['orientation'])
+        # print((ub-lb) * ratio + lb)
+        # print(ori)
+        trans = (ub-lb) * ratio + lb 
+        grasp = np.concatenate((trans, ori))
+        return grasp
 
 tt = python_wrapper_cpp.QpSolver()
 
-t1 = np.array([-0.704605, -0.0200731, 0.0205463, 0.730345, 0.059292, 0.0639174, 0.677492])
-t2 = np.array([-0.538106, -0.00185028, 0.0219165, -0.0582586, 0.677931, 0.730818, -0.0540426])
-t3 = np.array([-0.529481, -0.017858, 0.328078, 0.996136, 0.079409, 0.00298125, -0.0373972])
+idx_lists = [ 
+[12, 0.19341686393452673],
+[43, 0.32571935556810216],
+[48, 0.40444404066775363]] 
+
+grasp = ContinuousGraspCandid()
+
+t1 = grasp.get_grasp(idx_lists[0][0], idx_lists[0][1])
+t2 = grasp.get_grasp(idx_lists[1][0], idx_lists[1][1])
+t3 = grasp.get_grasp(idx_lists[2][0], idx_lists[2][1])
+
 rt = np.array([0,0,0,1])
 
 if tt.qp_solve_one_body(t1,t2,t3,rt):
